78e06250c536669df4c64f9cc636928fc0f0fa74
Mark Adams <Mark@fawkes.(none)>
2011-10-16 20:27:53 -0700

    Initial Commit

---
+++ Vox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Vox
+//
+//  Created by Mark Adams on 10/16/11.
+//  Copyright (c) 2011 Interstellar Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
