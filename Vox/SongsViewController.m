c6581a5208d6bc237992233d1509a7d8728c35c8
Mark Adams <Mark@fawkes.(none)>
2011-10-19 16:11:38 -0700

    Implemented UIKeyboardWillChangeFrameNotification to resize the table view to accommodate the keyboard when it is visible.

--- Vox/SongsViewController.m
+++ Vox/SongsViewController.m
@@ -22,7 +22,6 @@
 
 - (void)updateLeftBarButtonState;
 - (void)updateRightBarButtonState;
-- (void)updateNavigationItems;
 - (void)selectSongAtIndexPath:(NSIndexPath *)indexPath;
 - (void)configureCell:(SongCell *)cell forIndexPath:(NSIndexPath *)indexPath;
 
@@ -73,7 +72,8 @@
 - (void)setEditingSong:(BOOL)editingSongFlag
 {
     editingSong = editingSongFlag;
-    [self updateNavigationItems];
+    [self updateLeftBarButtonState];
+    [self updateRightBarButtonState];
 }
 
 #pragma mark - View Lifecycle
@@ -90,6 +90,24 @@
         [self addSong];
     else
         self.editingSong = NO;
+    
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    [notificationCenter addObserverForName:UIKeyboardWillChangeFrameNotification object:nil queue:nil usingBlock:^(NSNotification *notification) {
+        CGRect startingFrame = [[notification.userInfo valueForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+        CGRect endingFrame = [[notification.userInfo valueForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+        NSTimeInterval duration = [[notification.userInfo valueForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+        UIViewAnimationCurve animationCurve = [[notification.userInfo valueForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
+        
+        startingFrame = [self.view convertRect:startingFrame fromView:nil];
+        endingFrame = [self.view convertRect:endingFrame fromView:nil];
+        
+        CGRect viewFrame = self.tableView.frame;
+        viewFrame.size.height = endingFrame.origin.y - viewFrame.origin.y;
+        
+        [UIView animateWithDuration:duration delay:0.0 options:animationCurve animations:^{
+            self.tableView.frame = viewFrame;
+        } completion:nil];
+    }];
 }
 
 - (void)viewDidUnload
@@ -105,6 +123,7 @@
     [self updateRightBarButtonState];
 }
 
+#pragma mark - UI Helpers
 - (void)updateLeftBarButtonState
 {
     UIBarButtonItem *buttonItem = nil;
@@ -123,12 +142,6 @@
         buttonItem = self.addSongButton;
     
     [self.navigationItem setRightBarButtonItem:buttonItem animated:YES];
-}
-
-- (void)updateNavigationItems
-{
-    [self updateLeftBarButtonState];
-    [self updateRightBarButtonState];
 }
 
 #pragma mark - Add Song
