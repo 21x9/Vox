a81049f40f7ca6a8cd846613c046cb6837a9befe
Mark Adams <Mark@fawkes.(none)>
2011-10-20 01:06:41 -0700

    Moved stray classes into the group folder

---
+++ Vox/PlaceholderTextView.h
@@ -0,0 +1,15 @@
+//
+//  placeholderTextView.h
+//  Vox
+//
+//  Created by Mark Adams on 10/19/11.
+//  Copyright (c) 2011 Interstellar Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaceholderTextView : UITextView
+
+@property (copy, nonatomic) NSString *placeholder;
+
+@end
